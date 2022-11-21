54aac7b2a236418f845a08d1950344d2920b2aa9
Yuhananda Aditama <yuhananda.aditama@xsis.co.id>
2022-11-21 16:18:23 +0700

    add jacoco report

--- src/main/java/com/example/demo/models/Person.java
+++ src/main/java/com/example/demo/models/Person.java
@@ -34,9 +34,9 @@ public class Person {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
 
 	public String getNama() {
 		return nama;
